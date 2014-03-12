9a5df0e10811bb41a73a23e618da9c10da9ae02d
Lukasz Wojciak <lukasz.wojciak@pearson.com>
2014-03-12 12:02:29 +0100

    +Don't cluster - fork only

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 SCRIPTPATH=`pwd -P`
-pm2 start $SCRIPTPATH/node_modules/ep_etherpad-lite/node/server.js -i 1 --name etherpad
+pm2 start $SCRIPTPATH/node_modules/ep_etherpad-lite/node/server.js -i 1 -x --name etherpad
